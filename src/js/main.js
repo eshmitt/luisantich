13dde645889837e5f3c8b41ed61f31219671be89
eshmitt <32327938+eshmitt@users.noreply.github.com>
2018-12-13 20:37:06 +0000

    Updated
    was not commit as should have been on different rep, so had to transfer to new rep

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+// import styles, files and contact form
+import '../sass/main.scss';
+import Vue from 'vue';
+import VeeValidate from'vee-valitdate';
+import form from './compentents/form.vue';
+import '../index.html';
+import '../about.html';
+import '../contact.html';
+import '../nature.html';
+import '../people.html';
+import '../travel.html';
+import '../work.html';
+import axios from 'axios';
+
+Vue.use(VeeValidate);
+Vue.compnent('contact_form', form);
+
+function toggleNav() {
+    // call the nav
+    let nav = doument.getElementById("nav");
+    // call the nav as menu item clicked
+    document.getElementById.onclick = function() {
+    nav.style.display = "block";
+    };
+    // call exit to close nav
+    let exit = document.getElementById("exit")[0];
+    exit.onclick = function() {
+        nav.style.display = "none";
+    };
+}
+
+function toggleImg() {
+    // call the img overlat
+    let img = document.getElementById("img_overlay");
+    // call the img once clicked
+    document.getElementById.onclick = function() {
+        
+    }
+}
+
+window.onload(click());
+
+const contact_form = new Vue ({
+    el: '#contact_form',
+})
+
+function nameError() {
+}
